0aee92496692dbe5185effc3aeb07d0e83530916
HippoProgrammer <172101796+HippoProgrammer@users.noreply.github.com>
2026-09-12 19:50:04 +0100

    feature-fix: actually log event happenings

--- src/__main__.py
+++ src/__main__.py
@@ -607,9 +607,11 @@ async def on_thread_create(thread: discord.Thread): # ping the office when a new
             if thread.parent.get_tag(config["debate_tag_id"]) in thread.applied_tags:
                 embed = discord.Embed(title = "You have submitted your proposal into debate!", description = "You may motion your proposal to vote no sooner than 48 hours after the Flamewarden (or deputy) acknowledges the proposal.")
                 await thread.send(content=f"<@&{"> <@&".join(map(str, config["fw_announcement_role_ids"]))}>", embed=embed)
+                logger.info("Debate ping sent")
             else:
                 embed = discord.Embed(title = "You have submitted your proposal!", description = "You may motion your proposal to debate at any time by modifying this thread's tags to 'In Debate'.")
                 await thread.send(embed=embed)
+                logger.info("Submission embed sent")
 
 @bot.event
 async def on_thread_update(before: discord.Thread, after: discord.Thread): # ping the office when a new thread is created
@@ -619,6 +621,7 @@ async def on_thread_update(before: discord.Thread, after: discord.Thread): # pin
                 if after.parent.get_tag(config["debate_tag_id"]) in after.applied_tags and after.parent.get_tag(config["debate_tag_id"]) not in before.applied_tags:
                     embed = discord.Embed(title = "You have submitted your proposal into debate!", description = "You may motion your proposal to vote no sooner than 48 hours after the Flamewarden (or deputy) acknowledges the proposal.")
                     await after.send(content=f"<@&{"> <@&".join(map(str, config["fw_announcement_role_ids"]))}>", embed=embed)
+                    logger.info("Debate ping sent")
 
 @bot.event
 async def on_application_command_error(ctx:discord.ApplicationContext, error:discord.DiscordException):
