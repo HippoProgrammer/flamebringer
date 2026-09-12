f5fc46cb303847d9665a2e78acf4b3d693b0e6db
HippoProgrammer <172101796+HippoProgrammer@users.noreply.github.com>
2026-09-12 19:54:37 +0100

    feature-fix: add a relative timestamp in there as well

--- src/__main__.py
+++ src/__main__.py
@@ -469,7 +469,7 @@ async def acknowledge(ctx: discord.ApplicationContext):
     if permitted:
         logger.info("User is authenticated")
         conclusion = datetime.datetime.now() + datetime.timedelta(hours=int(config["debate_min_duration"]))
-        embed = discord.Embed(title = "Debate period acknowledged", description = f"The debate period has begun and will conclude at <t:{int(round(conclusion.timestamp(),0))}:f>, after which the proposal may be motioned to vote by any author.")
+        embed = discord.Embed(title = "Debate period acknowledged", description = f"The debate period has begun and will conclude at <t:{int(round(conclusion.timestamp(),0))}:f> (<t:{int(round(conclusion.timestamp(),0))}:R>), after which the proposal may be motioned to vote by any author.")
         await ctx.respond(embed = embed)
     else:
         logger.info("User is not authenticated")
