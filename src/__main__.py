6bac0125c0419fed98f516ebc18a229d547bdcde
HippoProgrammer <172101796+HippoProgrammer@users.noreply.github.com>
2026-09-12 19:46:30 +0100

    feature-fix: use correct ping syntax

--- src/__main__.py
+++ src/__main__.py
@@ -606,7 +606,7 @@ async def on_thread_create(thread: discord.Thread): # ping the office when a new
         if thread.can_send():
             if thread.parent.get_tag(config["debate_tag_id"]) in thread.applied_tags:
                 embed = discord.Embed(title = "You have submitted your proposal into debate!", description = "You may motion your proposal to vote no sooner than 48 hours after the Flamewarden (or deputy) acknowledges the proposal.")
-                await thread.send(content=f"<@{"> <@".join(map(str, config["fw_announcement_role_ids"]))}>", embed=embed)
+                await thread.send(content=f"<@&{"> <@&".join(map(str, config["fw_announcement_role_ids"]))}>", embed=embed)
             else:
                 embed = discord.Embed(title = "You have submitted your proposal!", description = "You may motion your proposal to debate at any time by modifying this thread's tags to 'In Debate'.")
                 await thread.send(embed=embed)
@@ -617,7 +617,7 @@ async def on_thread_update(before: discord.Thread, after: discord.Thread): # pin
         if after.can_send():
             if after.parent.get_tag(config["debate_tag_id"]) in after.applied_tags:
                 embed = discord.Embed(title = "You have submitted your proposal into debate!", description = "You may motion your proposal to vote no sooner than 48 hours after the Flamewarden (or deputy) acknowledges the proposal.")
-                await after.send(content=f"<@{"> <@".join(map(str, config["fw_announcement_role_ids"]))}>", embed=embed)
+                await after.send(content=f"<@&{"> <@&".join(map(str, config["fw_announcement_role_ids"]))}>", embed=embed)
 
 @bot.event
 async def on_application_command_error(ctx:discord.ApplicationContext, error:discord.DiscordException):
