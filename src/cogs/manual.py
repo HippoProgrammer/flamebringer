79f30b7904abb262be6d03402a347cff2fcca381
HippoProgrammer <172101796+HippoProgrammer@users.noreply.github.com>
2026-09-16 11:06:56 +0100

    fix: logging somewhere else

--- src/cogs/manual.py
+++ src/cogs/manual.py
@@ -1,12 +1,8 @@
 import discord
-from cogs.private import *
+import cogs.private as private
 import logging, sys
 
-logger = logging.getLogger("flamebringer")  # get the logger for this script
-handler = logging.StreamHandler(stream=sys.stdout)  # set logs to be sent to stdout
-formatter = logging.Formatter("%(asctime)s - %(module)s - %(levelname)s - %(message)s") # format [time] - [module] - [error level] - [message]
-handler.setFormatter(formatter) # attach the formatter to the handler
-logger.addHandler(handler)  # attach the handler to the logger
+logger = logging.getLogger(__name__)  # get the logger for this script
 logger.setLevel(logging.INFO)
 
 class Manual(discord.Cog):
